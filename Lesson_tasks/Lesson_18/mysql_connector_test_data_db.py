8ad4030a2eb787adb88a112a710d09371a87d39f
Vlad Lesyk <vlad.lesyk@gmail.com>
2022-12-11 22:52:21 +0200

    Commit lesson tasks 18

---
+++ Lesson_tasks/Lesson_18/mysql_connector_test_data_db.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+test_db = mysql.connector.connect(
+	host='localhost',
+	user='root',
+	password='03031995vlad',
+	database='testdata'
+)
+
+cursor = test_db.cursor()
+sql = 'insert into customers (name, address) values (%s, %s)'
+values = [
+	('Peter', 'Lowstreet 4'),
+	('Amy', 'Apple st 652'),
+	('Hannah', 'Mountain 21'),
+	('Michael', 'Valley 345'),
+	('Sandy', 'Ocean blvd 2'),
+	('Betty', 'Green Grass 1'),
+	('Richard', 'Sky st 331'),
+	('Susan', 'One way 98'),
+	('Vicky', 'Yellow Garden 2'),
+	('Ben', 'Park Lane 38'),
+	('William', 'Central st 954'),
+	('Chuck', 'Main Road 989'),
+	('Viola', 'Sideway 1633')
+]
+
+cursor.executemany(sql, values)
+test_db.commit()
---
+++ Lesson_tasks/Lesson_18/test_data_db.sql
@@ -0,0 +1,3 @@
+create database TestData;
+use testdata;
+create table customers (id int auto_increment primary key, name varchar(100), address varchar(255))
